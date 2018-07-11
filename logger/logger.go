5318e3a9a25917b54db5b34667fd22e97ad5c1d4
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2018-07-11 01:44:54 -0400

    mega update to logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -13,8 +14,10 @@ var LoggerDefaultDateFormat = time.RFC3339
 
 // Logger provides a middleware to log an incoming request.
 type Logger struct {
-	Service string
-	logger  *logrus.Logger
+	name   string
+	logger *logrus.Logger
+	before func(*logrus.Entry, *http.Request, string) *logrus.Entry
+	after  func(*logrus.Entry, ResponseWriter, time.Time, string) *logrus.Entry
 }
 
 // NewLogger creates a new AuthMiddleware with the given user session service.
@@ -23,8 +26,10 @@ func NewLogger(svc string) *Logger {
 	logger := logrus.New()
 	logger.Formatter = &logrus.JSONFormatter{}
 	return &Logger{
-		Service: svc,
-		logger:  logger,
+		name:   svc,
+		logger: logger,
+		before: DefaultBefore,
+		after:  DefaultAfter,
 	}
 }
 
@@ -33,21 +38,47 @@ func (l *Logger) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
-		l.logger.Infof("%s %s started", r.Method, r.URL)
+		entry := logrus.NewEntry(l.logger)
+		entry = l.before(entry, r, l.name)
+
+		entry.Info("starting request")
+
 		if r.Method == http.MethodOptions {
 			// router handles the OPTIONS request to obtain the list of allowed methods.
+			res := w.(ResponseWriter)
+			l.after(entry, res, start, l.name).Info("request completed")
 			next.ServeHTTP(w, r)
-			l.logger.Infof("%s %s | completed | took %.2fs", r.Method, r.URL, time.Since(start).Seconds())
 			return
 		}
 
 		next.ServeHTTP(w, r)
 
-		l.logger.Infof("%s: %s | took %.2fs", r.Method, r.URL, time.Since(start).Seconds())
+		res := w.(ResponseWriter)
+
+		l.after(entry, res, start, l.name).Info("request completed")
 	})
 }
 
 // ServeHTTP implements a negroni compatible signature.
 func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	l.Handle(next).ServeHTTP(w, r)
+}
+
+// DefaultBefore print log before request
+func DefaultBefore(entry *logrus.Entry, r *http.Request, name string) *logrus.Entry {
+	return entry.WithFields(logrus.Fields{
+		"service": name,
+		"method":  r.Method,
+		"URL":     r.URL,
+	})
+}
+
+// DefaultAfter print log after request
+func DefaultAfter(entry *logrus.Entry, res ResponseWriter, start time.Time, name string) *logrus.Entry {
+	return entry.WithFields(logrus.Fields{
+		"service":     name,
+		"status_code": res.Status(),
+		"status":      http.StatusText(res.Status()),
+		"took":        fmt.Sprintf("%.2fs", time.Since(start).Seconds()),
+	})
 }
