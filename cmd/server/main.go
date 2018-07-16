9e2cfeff2394d958cd1b8e881b9fcd5d1cfeacc2
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2018-07-16 15:32:25 -0400

    debug mode

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -86,9 +86,10 @@ func main() {
 	cors := cors.New(cors.Options{
 		AllowedOrigins:     []string{"*"}, // strings.Split(c.AllowedOrigins, ","),
 		AllowedHeaders:     []string{"Accept", "Authorization", "Content-Type"},
-		AllowedMethods:     []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"},
+		AllowedMethods:     []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS", "OPTION"},
 		AllowCredentials:   true,
 		OptionsPassthrough: true,
+		Debug:              true,
 	})
 
 	panic(http.ListenAndServe(fmt.Sprintf(":%s", *port), cors.Handler(r)))
