1fc63dbbafdbbf3814eb54b0362d0cd1e025346c
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2018-07-26 01:07:23 -0400

    improve logger

--- logger/response_writer.go
+++ logger/response_writer.go
@@ -5,8 +5,9 @@ import "net/http"
 // MyResponseWriter wraps a standard http.ResponseWriter
 // so we can store the status code.
 type MyResponseWriter struct {
-	status int
 	http.ResponseWriter
+	status      int
+	beforeFuncs []beforeFunc
 }
 
 // ResponseWriter ...
@@ -15,7 +16,12 @@ type ResponseWriter interface {
 	// Status returns the status code of the response or 0 if the response has
 	// not been written
 	Status() int
+	// Before allows for a function to be called before the ResponseWriter has been written to. This is
+	// useful for setting headers or any other operations that must happen before a response has been written.
+	Before(func(ResponseWriter))
 }
+
+type beforeFunc func(ResponseWriter)
 
 func newMyResponseWriter(res http.ResponseWriter) ResponseWriter {
 	mrw := &MyResponseWriter{
@@ -39,6 +45,11 @@ func (w *MyResponseWriter) Header() http.Header {
 	return w.ResponseWriter.Header()
 }
 
+// Before ...
+func (w *MyResponseWriter) Before(before func(ResponseWriter)) {
+	w.beforeFuncs = append(w.beforeFuncs, before)
+}
+
 func (w *MyResponseWriter) Write(data []byte) (int, error) {
 	return w.ResponseWriter.Write(data)
 }
@@ -47,8 +58,15 @@ func (w *MyResponseWriter) Write(data []byte) (int, error) {
 func (w *MyResponseWriter) WriteHeader(statusCode int) {
 	// Store the status code
 	w.status = statusCode
+	w.callBefore()
 	// Write the status code onward.
 	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *MyResponseWriter) callBefore() {
+	for i := len(w.beforeFuncs) - 1; i >= 0; i-- {
+		w.beforeFuncs[i](w)
+	}
 }
 
 type responseWriterCloseNotifer struct {
